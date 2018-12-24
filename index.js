f705f4b2cb0cda12853f89e7c1bc967a16626528
Igor Bykov <igorb.dev@websm.io>
2018-12-24 17:50:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const {spawn, spawnSync} = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+async function getStdinData() {
+
+    return new Promise((res, rej) => {
+
+        let returnData = '';
+        let dataHandler = data => { returnData += data; };
+
+        process.stdin.on('data', dataHandler);
+        process.stdin.on('end', _ => {
+            process.stdin.removeListener('data', dataHandler);
+            res(returnData);
+        });
+
+    });
+
+}
+
+async function cmdAdd(envData, stdinData) {
+
+    let containerId = envData.CNI_CONTAINERID;
+    let ifName = envData.CNI_IFNAME;
+
+    spawnSync('jexec', [
+        containerId, "ifconfig", ifName, "inet", "0.0.0.0", "up"
+    ]);
+    let result = spawnSync('dhclient-cni', [], {
+        input: JSON.stringify({
+            iface: ifName,
+            jid: parseInt(containerId),
+        }),
+    });
+
+    if (!result.status) {
+
+        let dhcp = JSON.parse(result.stdout);
+        let data = {
+            cniVersion: "0.3.1",
+            ips: [
+                {
+                    version: "4",
+                    address: dhcp.ip,
+                    gateway: dhcp.router,
+                },
+            ],
+            routes: [],
+            dns: {
+                nameservers: [ dhcp.options.nameserver ],
+                domain: dhcp.options.domain,
+            }
+        };
+
+        console.log(JSON.stringify(data));
+
+    } else {
+
+        let data = {
+            "cniVersion": "0.3.1",
+            "code": result.status,
+            "msg": result.output.toString(),
+        };
+
+        console.log(JSON.stringify(data));
+        process.exit(1);
+
+    }
+
+}
+
+async function cmdDel(envData, stdinData) {
+
+    let containerId = envData.CNI_CONTAINERID;
+    let ifName = envData.CNI_IFNAME;
+
+}
+
+// main
+(async _ => {
+
+    let defaults = {
+        cniVersion: '0.3.1',
+        name: '',
+        type: 'dhcp',
+    };
+
+    let envData = process.env;
+    let command = envData.CNI_COMMAND;
+
+    let stdinData = JSON.parse(await getStdinData());
+    stdinData = Object.assign({}, defaults, stdinData);
+
+    switch (command) {
+        case 'ADD':
+            await cmdAdd(envData, stdinData);
+            break;
+        case 'DEL':
+            await cmdDel(envData, stdinData);
+            break;
+    }
+
+})();
